3d0a67941b81f5940c364243402974ba6b6ea8b9
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-01-31 19:30:01 -0300

    Initial commit

---
+++ src/main/java/br/com/ada/moedas/compras/ordem/ApiOrdemCompraMoedas/ApiOrdemCompraMoedasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.moedas.compras.ordem.ApiOrdemCompraMoedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrdemCompraMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrdemCompraMoedasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/moedas/compras/ordem/ApiOrdemCompraMoedas/ApiOrdemCompraMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.moedas.compras.ordem.ApiOrdemCompraMoedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrdemCompraMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
