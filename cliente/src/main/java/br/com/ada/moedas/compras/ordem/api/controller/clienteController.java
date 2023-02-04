6bb6ff828dcee1cda36e6c74a2ff8b52cbd5bec0
MaylaSilva <may.siqueira93@gmail.com>
2023-02-04 18:39:15 -0300

    Entrega Sprint 1

---
+++ cliente/src/main/java/br/com/ada/moedas/compras/ordem/api/controller/clienteController.java
@@ -0,0 +1,24 @@
+package br.com.ada.moedas.compras.ordem.api.controller;
+
+import br.com.ada.moedas.compras.ordem.api.model.Cliente;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class clienteController {
+    @PostMapping("/{cpf}")
+    public ResponseEntity<Cliente> salvar(@PathVariable String cpf){
+        if (cpf == null || cpf == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma pessoa válida");
+        }
+        String clearCpf = cpf.replaceAll("[\\.-]", "");
+        if (clearCpf.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não foi informada uma pessoa com CPF válido");
+        }
+        Cliente teste = new Cliente(clearCpf,"João");
+        return ResponseEntity.ok(teste);
+    }
+}
---
+++ cliente/src/main/java/br/com/ada/moedas/compras/ordem/api/model/Cliente.java
@@ -0,0 +1,27 @@
+package br.com.ada.moedas.compras.ordem.api.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Cliente {
+    @Id
+    private String cpf;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String estadoCivil;
+    private String sexo;
+
+
+    public Cliente(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public Cliente() {
+
+    }
+}
