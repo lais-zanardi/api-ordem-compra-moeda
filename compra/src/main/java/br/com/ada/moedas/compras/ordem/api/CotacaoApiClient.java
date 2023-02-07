c75ae96499a5fb1c5b4cdc9eac70717fb818b34b
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-02-07 15:15:22 -0300

    Criação de CotacaoApiClient

---
+++ compra/src/main/java/br/com/ada/moedas/compras/ordem/api/CotacaoApiClient.java
@@ -0,0 +1,30 @@
+package br.com.ada.moedas.compras.ordem.api;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.remoting.RemoteAccessException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@RequiredArgsConstructor
+@Component
+public class CotacaoApiClient {
+    private final static String ENDPOINT = "http://{host}:{port}/api/cotacao/{moeda}";
+    private final EurekaClient eurekaClient;
+    private final RestTemplate restTemplate;
+
+    public boolean cotacaoDisponivel(String moeda) {
+        InstanceInfo apiInstanceInfo = eurekaClient
+                .getApplication("rooms-cotacao")
+                .getInstances()
+                .stream()
+                .findAny()
+                .orElseThrow(() -> new RemoteAccessException("Cotacao API Indisponível"));
+
+        return restTemplate
+                .getForEntity(ENDPOINT, String.class, apiInstanceInfo.getHostName(), apiInstanceInfo.getPort(), moeda)
+                .getStatusCode()
+                .is2xxSuccessful();
+    }
+}
