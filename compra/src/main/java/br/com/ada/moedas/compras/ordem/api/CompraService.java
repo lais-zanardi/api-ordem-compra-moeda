b2e00e13b43908e9e3155814a5b6e71b998238d4
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-02-07 15:25:11 -0300

    Verificação de duplicidade de entidade

--- compra/src/main/java/br/com/ada/moedas/compras/ordem/api/CompraService.java
+++ compra/src/main/java/br/com/ada/moedas/compras/ordem/api/CompraService.java
@@ -11,18 +11,23 @@ import java.util.Optional;
 public class CompraService {
     private final CrudRepository<Compra, Long> repository;
     private final ClienteApiClient clienteApiClient;
+    private final CotacaoApiClient cotacaoApiClient;
 
     public Optional<Compra> getById(Long id) {
         return repository.findById(id);
     }
 
-    public void incluir(Compra compra) {
+    public void incluir(Compra entity) {
+        if(repository.existsById(entity.getId())) {
+            throw new EntidadeDuplicadaException();
+        }
         repository.save(compra);
     }
 
 
     /**
      * Para realizar a compra de moedas é necessário:
+     * - Confirmar que compra não está duplicada (consulta repository e retorna exception caso esteja)
      * - Confirmar que o cliente existe (consultar API Cliente)
      * - Confirmar que a moeda consultada é USD ou EUR (consultar API Cotação)
      */
