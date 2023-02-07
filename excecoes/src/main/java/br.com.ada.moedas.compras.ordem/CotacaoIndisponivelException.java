8275d124c8cddc52c796c03ff0dd8eb8c89eb691
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-02-07 16:06:22 -0300

    Atualização de exception

--- excecoes/src/main/java/br.com.ada.moedas.compras.ordem/CotacaoIndisponivelException.java
+++ excecoes/src/main/java/br.com.ada.moedas.compras.ordem/CotacaoIndisponivelException.java
@@ -1,4 +1,7 @@
 package br.com.ada.moedas.compras.ordem;
 
 public class CotacaoIndisponivelException extends Throwable {
+    public CotacaoIndisponivelException(String message) {
+        super(message);
+    }
 }
