6e621c58c70afd8dfbab3ea5a3605fdcd84acfc3
MaylaSilva <may.siqueira93@gmail.com>
2023-02-08 20:23:02 -0300

    Correcao @bean e versão

--- compra/src/main/java/br/com/ada/moedas/compras/ordem/CompraApiApplication.java
+++ compra/src/main/java/br/com/ada/moedas/compras/ordem/CompraApiApplication.java
@@ -2,6 +2,8 @@ package br.com.ada.moedas.compras.ordem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CompraApiApplication {
@@ -10,4 +12,8 @@ public class CompraApiApplication {
 		SpringApplication.run(CompraApiApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
